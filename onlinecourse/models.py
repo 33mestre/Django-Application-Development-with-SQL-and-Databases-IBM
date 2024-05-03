8985ab18d161416f212d5fcf9e4c0c8f4e64bfd1
gideon.kipleting <chebei92@gmail.com>
2024-05-03 20:02:48 +0300

    a few edits on models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -10,7 +10,7 @@ from django.conf import settings
 import uuid
 
 
-# Instructor model
+#   Instructor model
 class Instructor(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
